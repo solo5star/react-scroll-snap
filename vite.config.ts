088ee97aba1ab5f12240e69af82366ddc23fac87
solo5star <solo_5@naver.com>
2023-10-17 17:28:48 +0900

    feat: project 초기 파일들 추가

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), dts({ rollupTypes: true })],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      formats: ['es'],
+      fileName: 'index',
+    },
+    rollupOptions: {},
+  },
+});
